c05e314a0a699a695cfa9c644ae8a5601f073fd3
Chabiu <chabinskijakub@gmail.com>
2024-01-24 22:00:43 +0100

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,7 +1,16 @@
-def readData():
-    pass
-
-print("Hello World")
-
-x = 5
-print(x)
+import datetime
+def wprowadz_kwote():
+    while True:
+            kwota= float(input("Wprowadź kwotę"))
+            if kwota < 0:
+                print("kwota nie może być ujemna")
+            else:
+                return kwota
+            
+def wprowadz_walute():
+    return input("Wprowadź kod waluty")
+def wprowadz_date():
+    while True
+        data_str=input("Wprowadz datę faktury  (RRRR-MM-DD):")
+        data=datetime.datetime.strptime(data_str, '&Y-%m-%d')
+     return data
