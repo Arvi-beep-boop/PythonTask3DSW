7d2d359e9f654479ce3b51908f1c6af68cab8299
Chabiu <chabinskijakub@gmail.com>
2024-01-24 23:10:26 +0100

    Update main.py

--- src/main.py
+++ src/main.py
@@ -10,7 +10,14 @@ def wprowadz_kwote():
 def wprowadz_walute():
     return input("Wprowadź kod waluty")
 def wprowadz_date():
-    while True
+    while True:
         data_str=input("Wprowadz datę faktury  (RRRR-MM-DD):")
-        data=datetime.datetime.strptime(data_str, '&Y-%m-%d')
-     return data
+        data=datetime.datetime.strtime(data_str, '%Y-%m-%d')
+        return data.strftime('%Y-%m-%d')
+ 
+def main():
+     kwota= wprowadz_kwote()
+     waluta= wprowadz_walute()
+     data=wprowadz_date()
+     print(f"Kwota faktury: {kwota} {waluta}")
+     print(f"Data faktury: {data}")
