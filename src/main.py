7e0e0d0277a7e298fb1e49e72c9f6a79945cd4f5
32blejd <filipiak.kacper000@gmail.com>
2024-01-26 20:22:26 +0100

    wprowadzanie platnosci

--- src/main.py
+++ src/main.py
@@ -1,7 +1,36 @@
-def readData():
-    pass
+import datetime
 
-print("Hello World")
+INVOICES = []
 
-x = 5
-print(x)
+def validate_date(date):
+    if date == "today":
+        return
+    else:
+        try: 
+            datetime.date.fromisoformat(date)
+        except ValueError:
+            raise ValueError("Incorrect data format, expected: YYYY-MM-DD")
+        
+def enter_date():
+    sample_date = input("Enter the invoice date (YYYY-MM-DD):")
+    validate_date(sample_date)
+
+# def enter_amount():
+    
+# def enter_currency():
+    
+# def get__data():
+    # date = get_date
+    # curr = get_currency
+    # amount = get_invoice
+    # temp = []
+    # temp.append(get_date())
+    # ....
+    # INVOICES.append(temp)
+
+
+    
+enter_date()
+
+#dodanie do tablicy 3 zmiennych (kwota, data, waluta)
+# 
